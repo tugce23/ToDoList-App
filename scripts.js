ae8678538ce0c93e45829e82cd0a5cdbfc9a504a
Tugce <tugceergur191@gmail.com>
2021-03-12 23:21:37 +0300

    Set and get item in localStorage

--- scripts.js
+++ scripts.js
@@ -1,3 +1,9 @@
+const todoList=localStorage.getItem('todoList').split(',');
+todoList.forEach((element)=>{
+    let todoList=document.getElementById('todo_list');
+    todoList.innerHTML+=`<li onclick="this.remove()">${element}</li>`
+})
+
 function addItem(){
     let inputBox=document.getElementById('todo_input');
     if(inputBox.value){
@@ -7,6 +13,8 @@ function addItem(){
         listItem.onclick=()=>{document.getElementById('todo_list').removeChild(listItem)};
         document.getElementById('todo_list').appendChild(listItem);
         inputBox.value="";
+        todoList.push(listItem.innerHTML);
+        localStorage.setItem('todoList',todoList)
     }else{
         alert('Input box cannot be empty')
     }

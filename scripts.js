94f0c538d40ba64c0669b174813a007f4f2b6ea5
Tugce <tugceergur191@gmail.com>
2021-03-12 21:28:43 +0300

    Fix empty input addition

--- scripts.js
+++ scripts.js
@@ -1,8 +1,13 @@
 function addItem(){
     let inputBox=document.getElementById('todo_input');
-    const listItem=document.createElement('li');
-    listItem.className='todo-list-item'
-    listItem.innerHTML=inputBox.value;
-    document.getElementById('todo_list').appendChild(listItem);
-    inputBox.value="";
+    if(inputBox.value){
+        const listItem=document.createElement('li');
+        listItem.className='todo-list-item'
+        listItem.innerHTML=inputBox.value;
+        document.getElementById('todo_list').appendChild(listItem);
+        inputBox.value="";
+    }else{
+        alert('Input box cannot be empty')
+    }
+    
 }
